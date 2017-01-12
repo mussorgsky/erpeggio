093c6b50d77707ac06ed86077fad88628dd9932b
mussorgsky <mateusz.gorzynski@gmail.com>
2017-01-12 22:19:58 +0100

    Added doors

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-    float x, y;
+typedef struct vec2 {
+    int x, y;
 } vec2;
 
-typedef struct {
+typedef struct door {
+    vec2 pos;
+    struct door *target;
+} door;
+
+typedef struct room {
     vec2 topLeft, dimensions;
+    int doorCount;
+    door *doors;
 } room;
 
-typedef struct {
+typedef struct level {
     vec2 size;
-    room *rooms;
     int roomCount;
+    room *rooms;
 } level;
 
 void initRoom(room *r, int tlX, int tlY, int w, int h) {
@@ -22,32 +29,65 @@ void initRoom(room *r, int tlX, int tlY, int w, int h) {
     r->dimensions.y = h;
 }
 
+#define scrnX 15
+#define scrnY 10
+char screen[scrnY][scrnX];
+
 int main() {
     level hub;
-    hub.size.x = 15;
-    hub.size.y = 10;
+    hub.size.x = scrnX;
+    hub.size.y = scrnY;
     hub.roomCount = 2;
     hub.rooms = malloc(hub.roomCount * sizeof(room));
 
-    initRoom(&hub.rooms[0], 1, 1, 5, 3);
-    initRoom(&hub.rooms[1], 8, 5, 5, 3);
+    initRoom(&hub.rooms[0], 1, 3, 6, 4);
+    initRoom(&hub.rooms[1], 8, 3, 6, 4);
 
-    int x, y, r;
-    room *tRoom;
-    for(y = 0; y < hub.size.y; y++) {
-        for(x = 0; x < hub.size.x; x++) {
-            int isRoom = 0;
-            for(r = 0; r < hub.roomCount; r++) {
-                tRoom = &hub.rooms[r];
-                if(y >= tRoom->topLeft.y && y <= tRoom->topLeft.y + tRoom->dimensions.y && x >= tRoom->topLeft.x && x <= tRoom->topLeft.x + tRoom->dimensions.x) {
-                    isRoom = 1;
-                }
+    hub.rooms[0].doorCount = 2;
+    hub.rooms[1].doorCount = 2;
+    hub.rooms[0].doors = malloc(hub.rooms[0].doorCount * sizeof(door));
+    hub.rooms[1].doors = malloc(hub.rooms[1].doorCount * sizeof(door));
+
+    hub.rooms[0].doors[0].pos.x = 5;
+    hub.rooms[0].doors[0].pos.y = 1;
+
+    hub.rooms[0].doors[1].pos.x = 5;
+    hub.rooms[0].doors[1].pos.y = 2;
+
+    hub.rooms[1].doors[0].pos.x = 0;
+    hub.rooms[1].doors[0].pos.y = 0;
+
+    hub.rooms[1].doors[1].pos.x = 0;
+    hub.rooms[1].doors[1].pos.y = 3;
+
+    /*
+        Screen displaying starts here
+    */
+    int x, y, i, j;
+    for(y = 0; y < scrnY; y++) {
+        for(x = 0; x < scrnX; x++) {
+            screen[y][x] = '.';
+        }
+    }
+
+    room *r;
+    door *d;
+    for(i = 0; i < hub.roomCount; i++) {
+        r = &hub.rooms[i];
+        for(y = r->topLeft.y; y < r->topLeft.y + r->dimensions.y; y++) {
+            for(x = r->topLeft.x; x < r->topLeft.x + r->dimensions.x; x++) {
+                screen[y][x] = ' ';
             }
-            if(isRoom) {
-                printf(" ");
-            } else {
-                printf("%c", 178);
-            }
+        }
+
+        for(j = 0; j < r->doorCount; j++) {
+            screen[r->topLeft.y + r->doors[j].pos.y][r->topLeft.x + r->doors[j].pos.x] = 'O';
+        }
+    }
+
+    for(y = 0; y < scrnY; y++) {
+        for(x = 0; x < scrnX; x++) {
+            printf("%c", screen[y][x]);
         }
         printf("\n");
     }
